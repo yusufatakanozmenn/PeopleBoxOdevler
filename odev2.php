542f58f4d7cb05f7b19e275c7d76ee667468039d
Yusuf Atakan Özmen <92385960+yusufatakanozmenn@users.noreply.github.com>
2024-05-17 23:44:46 +0300

    Update odev2.php

--- odev2.php
+++ odev2.php
@@ -78,7 +78,7 @@ const LIMIT = 85;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Film Listesi</title>
+    <title>Odev 2</title>
 </head>
 
 <body>
